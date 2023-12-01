feae14a5fee226cb8bb4aac20c744612c3b94c4f
sathishkumar <sathishm1701@gmail.com>
2023-12-01 17:04:45 +0530

    todo list using the useRef with tsx functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+import NewTodo from "./component/NewTodo";
 import Todos from "./component/Todos";
 import Todo from "./models/todo";
 function App() {
-  const todos = [new Todo("Learn React"),new Todo('Learn Typescript')];
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const addTodoHandler = (text: string) => {
+    setTodos((prevTodo) => {
+      return prevTodo.concat(new Todo(text));
+    });
+  };
+  const removeTodoHandler = (todoId: string) => {
+    setTodos((prevTodo) => {
+      return prevTodo.filter((todo) => todo.id !== todoId);
+    });
+  };
   return (
     <div>
-      <Todos items={todos} />
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos} removeHandler={removeTodoHandler} />
     </div>
   );
 }
