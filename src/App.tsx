47aa9e4cf18fff6d388abb20584f64b9d0739f1b
sathishkumar <sathishm1701@gmail.com>
2023-12-01 19:38:16 +0530

    change to useContext code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import { useState } from "react";
 import NewTodo from "./component/NewTodo";
 import Todos from "./component/Todos";
-import Todo from "./models/todo";
+import { TodoContextProvider } from "./store/ts-config";
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const addTodoHandler = (text: string) => {
-    setTodos((prevTodo) => {
-      return prevTodo.concat(new Todo(text));
-    });
-  };
-  const removeTodoHandler = (todoId: string) => {
-    setTodos((prevTodo) => {
-      return prevTodo.filter((todo) => todo.id !== todoId);
-    });
-  };
   return (
-    <div>
-      <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} removeHandler={removeTodoHandler} />
-    </div>
+    <TodoContextProvider>
+      <NewTodo />
+      <Todos />
+    </TodoContextProvider>
   );
 }
 
