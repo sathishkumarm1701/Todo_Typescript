7d5b4d1dfe34a2fcdffb732b67a5ff83ea9bead9
sathishkumar <sathishm1701@gmail.com>
2023-12-01 13:26:49 +0530

    todo initialized

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Todos from "./component/Todos";
+import Todo from "./models/todo";
+function App() {
+  const todos = [new Todo("Learn React"),new Todo('Learn Typescript')];
+  return (
+    <div>
+      <Todos items={todos} />
+    </div>
+  );
+}
+
+export default App;
